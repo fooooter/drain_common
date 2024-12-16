95a80a2051b27a44482e659b6477f636cca7afbc
footer <0xb00b1e5@proton.me>
2024-12-16 18:52:36 +0100

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+use std::collections::HashMap;
+
+pub enum RequestData<'a> {
+    Get {params: &'a Option<HashMap<String, String>>, headers: &'a HashMap<String, String>},
+    Post {headers: &'a HashMap<String, String>, data: &'a Option<HashMap<String, String>>},
+    Head {headers: &'a HashMap<String, String>}
+}
