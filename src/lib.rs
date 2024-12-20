a2b869b0854719290e29b847c17bacc07775e3d5
footer <0xb00b1e5@proton.me>
2024-12-20 12:33:18 +0100

    Make request_headers always present in the scope with shortening the RequestData enum.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod cookies;
 use std::collections::HashMap;
 
 pub enum RequestData<'a> {
-    Get {params: &'a Option<HashMap<String, String>>, headers: &'a HashMap<String, String>},
-    Post {headers: &'a HashMap<String, String>, data: &'a Option<HashMap<String, String>>},
-    Head {headers: &'a HashMap<String, String>}
+    Get(&'a Option<HashMap<String, String>>),
+    Post(&'a Option<HashMap<String, String>>),
+    Head
 }
