55c5aebc97cd6b7e272f7057d669e2faa57b82cb
footer <0xb00b1e5@proton.me>
2024-12-26 14:58:17 +0100

    Implement multipart/form-data.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,16 @@ use std::collections::HashMap;
 
 pub enum RequestData<'a> {
     Get(&'a Option<HashMap<String, String>>),
-    Post(&'a Option<HashMap<String, String>>),
+    Post(&'a Option<RequestBody>),
     Head
+}
+
+pub struct FormDataValue {
+    pub filename: Option<String>,
+    pub value: Vec<u8>
+}
+
+pub enum RequestBody {
+    XWWWFormUrlEncoded(HashMap<String, String>),
+    FormData(HashMap<String, FormDataValue>)
 }
