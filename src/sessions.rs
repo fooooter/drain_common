b773164079bebddf04c63e37a8b40890258a5aa3
footer <0xb00b1e5@proton.me>
2025-02-26 22:48:39 +0100

    Make sessions more optimal by more efficient lock management.

--- src/sessions.rs
+++ src/sessions.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use std::sync::{Arc, LazyLock};
 use openssl::base64;
 use openssl::rand::rand_priv_bytes;
-use tokio::sync::{Mutex, OwnedMutexGuard};
+use tokio::sync::Mutex;
 use crate::cookies::SameSite::Strict;
 use crate::cookies::{cookies, SetCookie};
 
@@ -11,23 +11,21 @@ pub trait SessionValue: Send {
     fn as_any(&self) -> &dyn Any;
 }
 
-static SESSIONS: LazyLock<Arc<Mutex<HashMap<String, Arc<Mutex<HashMap<String, Box<dyn SessionValue>>>>>>>> = LazyLock::new(|| {
-    Arc::new(Mutex::new(HashMap::new()))
+type SessionData = Arc<Mutex<HashMap<String, Box<dyn SessionValue>>>>;
+
+static SESSIONS: LazyLock<Mutex<HashMap<String, SessionData>>> = LazyLock::new(|| {
+    Mutex::new(HashMap::new())
 });
 
 pub struct Session {
-    session_key: String,
-    session_data: Arc<Mutex<HashMap<String, Box<dyn SessionValue>>>>,
-    sessions_ref: OwnedMutexGuard<HashMap<String, Arc<Mutex<HashMap<String, Box<dyn SessionValue>>>>>>
+    session_key: String
 }
 
 impl Session {
     pub async fn new(session_key: Option<String>, set_cookie: &mut HashMap<String, SetCookie>) -> Self {
-        let mut sessions = SESSIONS.clone().lock_owned().await;
-
         if let Some(session_key) = session_key {
-            if let Some(session_data) = sessions.get(&session_key) {
-                return Self { session_key, session_data: session_data.clone(), sessions_ref: sessions };
+            if SESSIONS.lock().await.contains_key(&session_key) {
+                return Self { session_key };
             }
         }
 
@@ -36,9 +34,8 @@ impl Session {
 
         let session_key = base64::encode_block(&session_id_bytes);
 
+        let mut sessions = SESSIONS.lock().await;
         sessions.insert(session_key.to_owned(), Arc::new(Mutex::new(HashMap::new())));
-
-        let session_data = sessions.get(&session_key).unwrap().clone();
 
         set_cookie.insert(String::from("SESSION_ID"), SetCookie {
             value: session_key.to_owned(),
@@ -52,29 +49,31 @@ impl Session {
             secure: false
         });
 
-        Self { session_key, session_data, sessions_ref: sessions }
+        Self { session_key }
     }
 
-    pub async fn set(&mut self, k: String, v: Box<dyn SessionValue>) {
-        self.session_data.lock().await.insert(k, v);
+    pub async fn set(&mut self, k: String, v: Box<dyn SessionValue>) -> bool {
+        if let Some(session) = SESSIONS.lock().await.get(&self.session_key) {
+            session.lock().await.insert(k, v);
+            return true;
+        }
+        false
     }
 
     pub async fn get<'a, V: SessionValue + Clone + 'static>(&'a self, k: &String) -> Option<V> {
-        if let Some(session_value) = self.session_data.lock().await.get(k) {
-            if let Some(session_value_extracted) = session_value.as_any().downcast_ref::<V>() {
-                return Some(session_value_extracted.clone())
+        if let Some(session) = SESSIONS.lock().await.get(&self.session_key) {
+            if let Some(session_value) = session.lock().await.get(k) {
+                if let Some(session_value_extracted) = session_value.as_any().downcast_ref::<V>() {
+                    return Some(session_value_extracted.clone())
+                }
             }
         }
         None
     }
 
-    pub fn session_key(&self) -> &String {
-        &self.session_key
-    }
+    pub fn session_key(&self) -> &String { &self.session_key }
 
-    pub async fn destroy(mut self) {
-        self.sessions_ref.remove(&self.session_key);
-    }
+    pub async fn destroy(self) { SESSIONS.lock().await.remove(&self.session_key); }
 }
 
 pub async fn start_session(request_headers: &HashMap<String, String>, set_cookie: &mut HashMap<String, SetCookie>) -> Session {
